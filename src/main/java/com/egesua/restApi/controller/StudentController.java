1ad17c2fed33031f8ef12286429d1c267ef07e78
egesua <egesu06@gmail.com>
2024-04-24 15:42:26 +0300

    student controller completed(sort of)

--- src/main/java/com/egesua/restApi/controller/StudentController.java
+++ src/main/java/com/egesua/restApi/controller/StudentController.java
@@ -24,4 +24,17 @@ public class StudentController {
        return students.values().stream().toList();
     }
 
+    @PutMapping("/{id}")
+    public Student update(@PathVariable Long id, @RequestBody Student student) {
+            students.put(id, new Student(id, student.getFirstName(), student.getLastName(), student.getGrade(), student.isSuccessful()));
+        return students.get(id);
+    }
+
+    @DeleteMapping("/{id}")
+    public Student delete(@PathVariable Long id) {
+        Student student = students.get(id);
+        students.remove(student);
+        return student;
+    }
+
 }
