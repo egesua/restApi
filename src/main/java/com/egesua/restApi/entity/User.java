2cc90f5b8667100f1a00f4980739e807f73df2ed
egesua <egesu06@gmail.com>
2024-04-24 14:36:46 +0300

    entity created.

---
+++ src/main/java/com/egesua/restApi/entity/User.java
@@ -0,0 +1,17 @@
+package com.egesua.restApi.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    private String id;
+    private String name;
+    private String address;
+    private String number;
+
+
+}
